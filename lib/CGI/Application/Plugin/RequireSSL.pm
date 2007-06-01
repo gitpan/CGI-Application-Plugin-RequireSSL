8fba211ba73ecebe0a07aac8c6295c88d35142a3
Dan Horne <dhorne@cpan.org>
2007-06-01 00:18:23 +0000

    Import of DHORNE/CGI-Application-Plugin-RequireSSL-0.02 from CPAN.
    gitpan-cpan-distribution: CGI-Application-Plugin-RequireSSL
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DHORNE/CGI-Application-Plugin-RequireSSL-0.02.tar.gz
    gitpan-cpan-author:       DHORNE
    gitpan-cpan-maturity:     released

--- lib/CGI/Application/Plugin/RequireSSL.pm
+++ lib/CGI/Application/Plugin/RequireSSL.pm
@@ -14,11 +14,11 @@ CGI::Application::Plugin::RequireSSL - Force SSL in specified pages or modules
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
